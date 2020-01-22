42527cd1fb98995e24418c28251c0295d90ab3fc
Rui Chen <chenrui333@gmail.com>
2020-01-22 15:42:54 -0500

    opa 0.16.2
    Closes #49301.
    
    Signed-off-by: Rui Chen <chenrui333@gmail.com>

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -4,8 +4,8 @@ require_relative "../std_requirements/std_go_requirement"
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/v0.16.1.tar.gz"
-  sha256 "2251b905e7e6c416b71800bd779b184bce2302bd4655666c6e35e6d5c0e274b9"
+  url "https://github.com/open-policy-agent/opa/archive/v0.16.2.tar.gz"
+  sha256 "f71b0532790962fb2b19b18acab21f8303c1e25f1a6822f461f006a3f47f0c93"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles" # https://github.com/Homebrew/brew/issues/6059
