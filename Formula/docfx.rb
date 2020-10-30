3bba010a86f7d280bc62bd26013f4183a029b91d
nfon-christophe <54438034+nfon-christophe@users.noreply.github.com>
2020-10-30 11:09:07 +0000

    Update docfx.rb
    Link to most recent Docfx version 2.56.4

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -4,7 +4,8 @@ require_relative "../std_requirements/std_mono_requirement"
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.51/docfx.zip"
+  #url "https://github.com/dotnet/docfx/releases/download/v2.51/docfx.zip"
+  url "https://github.com/dotnet/docfx/releases/download/v2.56.4/docfx.zip"
   sha256 "feaab69b369c55cd27f9f1a6c31c3279dd309ee61db030d93201c44fcbcd3672"
 
   bottle :unneeded
