c24312a16aded4004b42ef0e34069da64986e494
nfon-christophe <54438034+nfon-christophe@users.noreply.github.com>
2020-10-30 11:12:40 +0000

    Update docfx.rb
    Updating SHA256 Value

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -6,7 +6,8 @@ class Docfx < Formula
   homepage "https://dotnet.github.io/docfx/"
   #url "https://github.com/dotnet/docfx/releases/download/v2.51/docfx.zip"
   url "https://github.com/dotnet/docfx/releases/download/v2.56.4/docfx.zip"
-  sha256 "feaab69b369c55cd27f9f1a6c31c3279dd309ee61db030d93201c44fcbcd3672"
+  #sha256 "feaab69b369c55cd27f9f1a6c31c3279dd309ee61db030d93201c44fcbcd3672"
+  sha256 "4ed09c7e6dfed3a579a8a3d1678db270211064a1ffc884e865b7a74ad1adfb27"
 
   bottle :unneeded
 
