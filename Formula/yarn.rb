24bb6a8e6b6438fa3825311a8d9e43a66ab1e123
Sam Ford <1584702+samford@users.noreply.github.com>
2021-01-09 12:47:50 -0500

    yarn: update livecheck (#68602)
    # Conflicts:
    #	Formula/yarn.rb

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -9,6 +9,10 @@ class Yarn < Formula
   sha256 "7e433d4a77e2c79e6a7ae4866782608a8e8bcad3ec6783580577c59538381a6e"
   license "BSD-2-Clause"
 
+  livecheck do
+    skip("1.x line is frozen and features/bugfixes only happen on 2.x")
+  end
+
   bottle :unneeded
 
   depends_on "node" => :optional # Make node optional so it's not installed by default & we get --with-node
